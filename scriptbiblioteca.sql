d76661f8bfdd73f562f789dbfe80bff521bfd644
juliafreitas1 <70281797+juliafreitas1@users.noreply.github.com>
2022-04-27 20:26:37 -0300

    Add files via upload

---
+++ scriptbiblioteca.sql
@@ -0,0 +1,24 @@
+drop table cadlivros;
+drop table cadusuariosdabiblioteca;
+drop table emprestimos;
+CREATE TABLE cadlivros(
+id serial primary key,
+titulo varchar(100)
+);
+CREATE TABLE cadusuariosdabiblioteca(
+id serial primary key,
+nome varchar(100) not null
+);
+CREATE TABLE emprestimos(
+id serial primary key,
+id_do_livro integer,
+id_do_usuario integer
+datadoemprestimo date
+);select * from emprestimos;insert into cadlivros (titulo) values ('1984');
+insert into cadlivros (titulo) values ('Revolucao dos bichos');
+select * from cadlivros;
+insert into cadusuariosdabiblioteca (id,nome) values (1,'Joao');
+insert into cadusuariosdabiblioteca (id,nome) values (2,'Maria');
+insert into cadusuariosdabiblioteca (id,nome) values (3,'David');
+select * from cadusuariosdabiblioteca;
+
